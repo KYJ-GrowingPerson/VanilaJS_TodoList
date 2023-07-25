d7c47aab9eb681f528b027a49ffa8b709df0e44c
YJUN <120626955+YJUN99@users.noreply.github.com>
2023-07-25 17:22:20 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,7 @@
 function onGeoSuccess(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const API_KEY = "ed6a04e2d546d1438570fbdb5179980f";
+    const API_KEY = "yourKEY";
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
     const weatherForm = document.getElementById("weatherForm");
     
